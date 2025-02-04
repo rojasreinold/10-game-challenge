66c7d0e7146b8c1510dced5927140c5b0a10fcd0
Reinold Rojas <rojasreinold@gmail.com>
2025-02-04 18:34:42 -0500

    Added ball moving to p1

--- 1-pong/main.gd
+++ 1-pong/main.gd
@@ -1,11 +1,21 @@
 extends Node
-
+@export var ball_scene : PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	new_game()
+	$Ball.hide()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
+
+func new_game():
+	#var ball = ball_scene.instantiate()
+	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	$Ball.linear_velocity = Vector2(-250, 0.0)
+	
+func game_end():
 	pass
