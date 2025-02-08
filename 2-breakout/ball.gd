5a15eecb7ef692350b3dee7f5430e2ebee1d8d00
Reinold Rojas <rojasreinold@gmail.com>
2025-02-08 17:31:58 -0500

    Created a row of blocks

--- 2-breakout/ball.gd
+++ 2-breakout/ball.gd
@@ -1,11 +1,25 @@
 extends Area2D
 
+signal ball_missed
+
+const DEFAULT_SPEED = 400
+
+var direction := Vector2.DOWN
+var _speed := DEFAULT_SPEED
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	area_entered.connect(_on_area_entered)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	_speed += delta * 2
+	position += _speed * delta * direction
+	
+func _on_area_entered(area: Area2D) -> void:
+	if area.name == "Player1":
+		direction = Vector2(randf_range(-1,1), -1)
+	if area.name == "Block":
+		direction = Vector2(randf_range(-1,1), 1)
--- 2-breakout/main.gd
+++ 2-breakout/main.gd
@@ -1,11 +1,26 @@
 extends Node
-
+var block_scene = preload("res://block.tscn")
+var screen_size
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	screen_size = get_viewport().size
+	new_game()
+
+	print(screen_size)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func new_game() -> void:
+	
+	#loop creating a row. 14 nodes. block is 100
+	var block_horizontal_space = 4
+	for n in range(0, screen_size[0] , 100 + 4):
+		var block = block_scene.instantiate()
+		block.position = Vector2(n, 0)
+		add_child(block)
+		print("creating block")
