bd0385596ee00a270d00967c8abb6151211c99dd
Reinold Rojas <rojasreinold@gmail.com>
2025-02-14 15:01:36 -0500

    Road lane spawning enemies

--- 3-frogger/enemy.gd
+++ 3-frogger/enemy.gd
@@ -3,12 +3,11 @@ extends RigidBody2D
 #class_name enemy
 
 var movement: Vector2 = Vector2.ZERO
-
-
+var direction: int = 1
+	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$VisibleOnScreenNotifier2D.screen_exited.connect(_on_visible_on_screen_notifier_2d_screen_exited)
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,9 +15,13 @@ func _process(delta: float) -> void:
 	pass
 
 
+func set_direction(new_dir: int) -> void:
+	if direction != new_dir:
+		$Sprite2D.flip_h = not $Sprite2D.flip_h
+	direction = new_dir
+	
 func move() -> void:
-	position = Vector2(position.x- 16, position.y)
-	pass
+	position = Vector2(position.x+16 * direction, position.y)
 	
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
--- 3-frogger/enemy_spawn_point.gd
+++ 3-frogger/enemy_spawn_point.gd
@@ -1,11 +1,20 @@
 extends Marker2D
 class_name EnemySpawnPoint
 
-var car = load("res://car_enemy.tscn")
+var road_array = Array()
+var water_array = Array()
+@export_enum("LEFT:-1", "RIGHT:1") var direction: int
+
+func _init() -> void:	
+	var car = load("res://car_enemy.tscn")
+	var gator = load("res://gator_enemy.tscn")
+	road_array.append(car)
+	water_array.append(gator)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$SpawnTimer.timeout.connect(spawn_enemy)
+	spawn_enemy()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,6 +22,7 @@ func _process(delta: float) -> void:
 	pass
 	
 func spawn_enemy() -> void:
-	var new_car = car.instantiate()
+	var new_car = road_array[0].instantiate()
+	new_car.set_direction(direction)
 	$MoveTimer.timeout.connect(new_car.move)
 	add_child(new_car)
