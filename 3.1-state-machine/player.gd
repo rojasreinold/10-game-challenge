c0ec9db4abbbd7dc2f76c2ecc2f9adcbab60d08a
Reinold Rojas <rojasreinold@gmail.com>
2025-02-18 18:34:56 -0500

    setting functin

--- 3.1-state-machine/player.gd
+++ 3.1-state-machine/player.gd
@@ -1,29 +1,55 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+var SPEED: int = 300.0
 const JUMP_VELOCITY = -400.0
-enum States {IDLE, RUNNING, JUMPING, FALLING, GLIDING}
 
-var state: States = States.IDLE
+var speed := 500.0
+var jump_impulse := 1800.0
+var base_gravity := 4000.0
+
+var glide_max_speed := 1000.0
+var glide_acceleration := 1000.0
+var glide_gravity := 400.0
+var glide_jump_impulse := 800.0
+
+var current_gravity := base_gravity
+
+enum States {IDLE, RUNNING, JUMPING, FALLING, GLIDING}
+const GROUND_STATES := [States.IDLE, States.RUNNING]
+
+var state: States = States.IDLE: set = set_state
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	var input_direction_x: int = Input.get_axis("move_left", "move_right")
+	if state in GROUND_STATES and Input.is_action_just_pressed("jump"):
+		state = States.JUMPING
+	elif state in GROUND_STATES and not is_on_floor():
+		state = States.FALLING
+	elif is_on_floor():
+		if input_direction_x != 0.0:
+			state = States.RUNNING
+		else:
+			state = States.IDLE
+	
+	
+	#if not is_on_floor():
+		#velocity += get_gravity() * delta
+#
+	## Handle jump.
+	#if Input.is_action_just_pressed("jump") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
+#
+	## Get the input direction and handle the movement/deceleration.
+	## As good practice, you should replace UI actions with custom gameplay actions.
+	#var direction := Input.get_axis("move_left", "move_right")
+	#if direction:
+		#velocity.x = direction * SPEED
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	velocity.y += current_gravity * delta
 	move_and_slide()
 	
 	
@@ -42,3 +68,16 @@ func update_state_label() -> void:
 			state_string = "gliding"
 		
 	$StateLabel.text = state_string
+	
+	
+func set_state(new_state: int) -> void:
+	print("setting state")
+	var previous_state: int = state
+	state = new_state
+	
+	if state == States.IDLE:
+		velocity.x = 0.0
+	elif state == States.JUMPING:
+		velocity.y = -jump_impulse
+		
+	update_state_label()
