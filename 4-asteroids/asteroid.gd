48f778993d0520d6a43d3dd1e1e06daff799afcd
Reinold Rojas <rojasreinold@gmail.com>
2025-05-08 23:43:47 -0400

    initial asteroid screen wrapping

--- 4-asteroids/asteroid.gd
+++ 4-asteroids/asteroid.gd
@@ -6,3 +6,20 @@ var SPEED = 200
 func set_direction(new_direction):
 	linear_velocity.x = SPEED* cos(new_direction)
 	linear_velocity.y = SPEED * sin(new_direction)
+
+
+func _physics_process(delta: float) -> void:
+	var window_size = DisplayServer.window_get_size()
+	print(window_size, global_position)
+	if global_position.x < 0:
+		global_position.x = window_size.x
+	elif global_position.y > window_size.x:
+		global_position.x = 0
+	
+	if global_position.y < 0:
+		global_position.y = window_size.y
+	elif global_position.y > window_size.y:
+		print("y change")
+		global_position.y = 0
+
+#func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
--- 4-asteroids/main.gd
+++ 4-asteroids/main.gd
@@ -5,10 +5,8 @@ extends Node2D
 func _on_asteroid_timer_timeout() -> void:
 	var  AsteroidPathFollow: PathFollow2D = $AsteroidPath/AsteroidPathFollow2D
 	AsteroidPathFollow.progress_ratio = randf()
-	print(AsteroidPathFollow.get_progress())
 	var direction = AsteroidPathFollow.rotation + PI/2
 	direction += randf_range(-PI/4, PI/4)
-	print("direction: ", rad_to_deg(direction))
 	var asteroid = asteroid_scene.instantiate()
 	asteroid.set_direction(direction)
 	asteroid.set_position(AsteroidPathFollow.position)
