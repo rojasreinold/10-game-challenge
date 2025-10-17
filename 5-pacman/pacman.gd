9e5922d048a9a82dad905d4eea0531554f293de5
Reinold Rojas <rojasreinold@gmail.com>
2025-10-16 20:30:27 -0400

    Better movement logic

--- 5-pacman/pacman.gd
+++ 5-pacman/pacman.gd
@@ -6,15 +6,20 @@ func _process(delta: float) -> void:
 	var horizontal_movement = Input.get_axis("ui_left", "ui_right")
 	var vertical_movement = Input.get_axis("ui_up", "ui_down")
 	
+	var new_direction = Vector2.ZERO
 	if horizontal_movement != 0:
-		direction.x = horizontal_movement
+		new_direction.x = horizontal_movement
 		velocity = direction * BASE_SPEED
 	elif vertical_movement != 0:
-		direction.y = vertical_movement
+		new_direction.y = vertical_movement
 		velocity = direction * BASE_SPEED
 
 	else:
-		velocity = direction * BASE_SPEED
+		new_direction = direction
+		
+	direction = new_direction
+	velocity = direction * BASE_SPEED
 
 	#print(velocity)
 	move_and_slide()
+	#move_and_collide(velocity)
